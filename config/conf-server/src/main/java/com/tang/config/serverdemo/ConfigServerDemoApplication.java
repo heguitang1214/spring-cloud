cba3142e6cc082d9d498c8af8b8e399f5df7b2eb
Tang <he_guitang@163.com>
2019-06-03 14:36:08 +0800

    Spring cloud基本组件的使用

---
+++ config/conf-server/src/main/java/com/tang/config/serverdemo/ConfigServerDemoApplication.java
@@ -0,0 +1,18 @@
+package com.tang.config.serverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Config配置中心启动类 加上@EnableConfigServer注解
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerDemoApplication.class, args);
+	}
+
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
-# Spring Cloud
+# Spring Cloud Demo
+* 搭建SpringCloud各组件使用的demo
 
 ## 端口设置
-* 配置中心：
-* 注册中心：
+* 配置中心config server：11000
+* 注册中心eureka：8761
+* 服务的消费者consumer：909
+* 服务的提供者provider：8080
+* 网关zuul：9999
